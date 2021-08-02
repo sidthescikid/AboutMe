c590813a76789e7d97099e8169f3c7251adb556e
sidthescikid <hoquesy@gmail.com>
2021-08-02 14:34:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function logoHover0() {
+    document.getElementById("period").innerHTML = '\267'
+    
+}
+
+function logoHover1() {
+    document.getElementById("period").innerHTML = ''
+}
+
+function useName() {
+    var name = document.getElementById("firname").value;
+    document.getElementById("nameInput").innerHTML = name;
+}
+
